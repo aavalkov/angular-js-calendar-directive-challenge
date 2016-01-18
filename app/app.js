70350e80f668753d8b33375449bc5073ca13ba89
Anna Valkov <anna.a.valkov@gmail.com>
2016-01-18 10:31:41 -0800

    create calendar nav

--- app/app.js
+++ app/app.js
@@ -1,3 +1,28 @@
-angular.module('calendarDemoApp', []);
+var app = angular.module('calendarDemoApp', []);
+app.directive('calendar', function(){
+	return {
+		templateUrl: 'calendar.html',
+		restrict: 'E',
+		scope: true,
+        transclude: true,
+		link: function(scope, element, attrs){
 
-// your controller and directive code go here
+		},
+		controller: function($scope, $element, $attrs){
+			var today = new Date();
+			var currentMonth = today.getMonth();
+			var currentYear = today.getFullYear();
+
+			$scope.currentMonth = currentMonth;
+			$scope.currentYear = currentYear;
+
+			$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+			$scope.years = [];
+
+			for (var i = 0; i <= 40; i++){
+				$scope.years.push(currentYear - 20 + i);
+			}
+		}
+	}
+});
+
