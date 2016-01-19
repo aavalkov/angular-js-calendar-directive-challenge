0c8404a1d4e12b1e8b7512aee3153a2b2f8cef50
Anna Valkov <anna.a.valkov@gmail.com>
2016-01-18 20:20:00 -0800

    create functionality to change calendar when date changes

--- app/app.js
+++ app/app.js
@@ -12,16 +12,26 @@ app.directive('calendar', function(){
 			var today = new Date();
 			var currentMonth = today.getMonth();
 			var currentYear = today.getFullYear();
-
+			
 			$scope.currentMonth = currentMonth;
 			$scope.currentYear = currentYear;
-
+			
+			$scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 			$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
 			$scope.years = [];
 
 			for (var i = 0; i <= 40; i++){
 				$scope.years.push(currentYear - 20 + i);
 			}
+
+			$scope.selectedMonth = currentMonth;
+			$scope.selectedYear = currentYear;
+
+			$scope.newCalendar = function(){
+				$scope.range = CalendarRange.getMonthlyRange(new Date($scope.selectedYear, $scope.selectedMonth));
+			}
+
+
 		}
 	}
 });
